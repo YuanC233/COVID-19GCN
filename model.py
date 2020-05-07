fd26ffe4dbfbd7d9315301775f1c65c645b9ec1f
TachyonFTL <yifan233@utexas.edu>
2020-05-06 22:28:54 -0400

    update model

--- model.py
+++ model.py
@@ -3,22 +3,26 @@ import torch
 import torch.nn as nn
 import dgl
 import dgl.function as fn
+from torch.nn import functional as F
 
 
 def msg_func(edges):
-    return {'m': edges.data['weight'].unsqueeze(1) * edges.src['h']}
+    # s = list(edges.src['h'].size())[-1]
+    # return {'m': edges.data['edge_mat'].view(-1, s, s).matmul(edges.src['h'].view(s, -1)).view(-1, s)}
+    return {'m': edges.data['eh'] + edges.src['h']}
 
 
 class GCNLayer(nn.Module):
     def __init__(self,
                  in_feats,
-                 out_feats,
+                 out_feats, edge_hidden_dim,
                  activation,
                  dropout,
                  bias=True):
         super(GCNLayer, self).__init__()
 
         self.weight = nn.Parameter(torch.Tensor(in_feats, out_feats))
+        self.weight_edge = nn.Parameter(torch.Tensor(edge_hidden_dim, in_feats))
         if bias:
             self.bias = nn.Parameter(torch.Tensor(out_feats))
         else:
@@ -35,7 +39,9 @@ class GCNLayer(nn.Module):
 
     def reset_parameters(self):
         stdv = 1. / math.sqrt(self.weight.size(1))
+        stdv_edge = 1. / math.sqrt(self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
+        self.weight_edge.data.uniform_(-stdv_edge, stdv_edge)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
 
@@ -44,11 +50,13 @@ class GCNLayer(nn.Module):
             h = self.dropout(h)
 
         g.srcdata['h'] = h
+        g.edata['eh'] = torch.mm(g.edata['edge_feat'], self.weight_edge)
         g.update_all(msg_func, fn.sum(msg='m', out='h'))
         h = g.dstdata['h']
         # bias
 
         h = torch.mm(h, self.weight)
+
         if self.norm != 'none':
             degs = g.in_degrees().to(h.device).float().clamp(min=1)
             if self.norm == 'both':
@@ -71,29 +79,70 @@ class GCN(nn.Module):
                  num_element,
                  embedding_dim,
                  hidden_dim,
+                 edge_hidden_dim,
                  num_classes,
                  num_layers,
                  activation,
                  dropout):
         super(GCN, self).__init__()
         self.element_emebdding = nn.Embedding(num_element, embedding_dim)
+        self.edge_emebdding = nn.Embedding(4, edge_hidden_dim)
         self.layers = nn.ModuleList()
         # input layer
-        self.layers.append(GCNLayer(embedding_dim, hidden_dim, activation, False))
+        self.layers.append(GCNLayer(embedding_dim, hidden_dim, edge_hidden_dim, activation, False))
         # hidden layers
         for i in range(num_layers - 1):
-            self.layers.append(GCNLayer(hidden_dim, hidden_dim, activation, dropout))
+            self.layers.append(GCNLayer(hidden_dim, hidden_dim, edge_hidden_dim, activation, dropout))
         # output layer
         self.out = nn.Linear(hidden_dim, num_classes)
+        self.hidden_dim = hidden_dim
 
     def forward(self, g):
         h = self.element_emebdding(g.ndata['elem_idx'])
+        g.edata['edge_feat'] = self.edge_emebdding(g.edata['order'])
+
         for layer in self.layers:
             h = layer(h, g)
+
         g.ndata['h'] = h
-        # Calculate graph representation by averaging all the node representations.
         hg = dgl.mean_nodes(g, 'h')
         return self.out(hg)
+
+
+class MLP(nn.Module):
+    def __init__(self,
+                 num_element,
+                 embedding_dim,
+                 hidden_dim,
+                 num_classes,
+                 num_layers,
+                 dropout):
+        super(MLP, self).__init__()
+        self.element_emebdding = nn.Embedding(num_element, embedding_dim, padding_idx=0)
+        self.layers = nn.ModuleList()
+        # input layer
+        self.layers.append(nn.Linear(embedding_dim, hidden_dim))
+        # hidden layers
+        for i in range(num_layers - 1):
+            self.layers.append(nn.Linear(hidden_dim, hidden_dim))
+        # output layer
+        self.out = nn.Linear(hidden_dim, num_classes)
+        if dropout:
+            self.dropout = nn.Dropout(p=dropout)
+        else:
+            self.dropout = False
+
+    def forward(self, x, n_atoms):
+        h = self.element_emebdding(x)
+        h = torch.sum(h, dim=1) / n_atoms.unsqueeze(1)
+
+        for layer in self.layers:
+            h = layer(h)
+            h = F.relu(h)
+            if self.dropout:
+                h = self.dropout(h)
+
+        return self.out(h)
 
 
 if __name__ == '__main__':
@@ -101,38 +150,59 @@ if __name__ == '__main__':
     import torch.optim as optim
     from torch.utils.data import DataLoader
     from gcn_utils import collate
-    from utils import get_data
+    from utils import get_data, get_data_for_mlp
     from tqdm import tqdm
-
-    train_data = get_data('train.csv', device='cuda:0')
-    train_data = get_data('train.csv', device='cuda:0')
-    data_loader = DataLoader(train_data, batch_size=32, shuffle=True, collate_fn=collate)
-
-    model = GCN(27, 16, 48, 2, 3, F.relu, 0.1)
-    loss_func = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    model.train()
-    device = torch.device('cuda:0')
-    model.to(device)
-
-    for epoch in range(80):
-        epoch_loss = 0
-        batch = tqdm(data_loader)
-        for bg, label in batch:
-            optimizer.zero_grad()
-            prediction = model(bg)
-            loss = torch.mean(F.cross_entropy(prediction, label, reduction='none')
-                             * (torch.ones_like(label, dtype=torch.float, device=device) +
-                                torch.tensor(label.clone().detach(), dtype=torch.float, device=device) * 10))
-
-            loss.backward()
-            optimizer.step()
-            epoch_loss += loss.detach().item()
-            batch.set_description(f'epoch {epoch} loss {epoch_loss}')
-        if epoch > 10:
-            print(F.softmax(prediction.detach()).cpu())
-            print(label.cpu())
-
+    #
+    # train_data = get_data('train_cv/fold_0/train.csv', device='cuda:0')
+    # dev_data = get_data('train_cv/fold_0/dev.csv', device='cuda:0')
+    # data_loader = DataLoader(train_data, batch_size=32, shuffle=True, collate_fn=collate)
+    # _dev_data_loader = DataLoader(dev_data, batch_size=32, shuffle=False, collate_fn=collate)
+    #
+    # model = GCN(27, 16, 48, 2, 3, F.relu, 0.1)
+    # loss_func = nn.CrossEntropyLoss()
+    # optimizer = optim.Adam(model.parameters(), lr=0.001)
+    #
+    # device = torch.device('cuda:0')
+    # model.to(device)
+    #
+    # for epoch in range(80):
+    #     model.train()
+    #     epoch_loss = 0
+    #     batch = tqdm(data_loader)
+    #     for bg, label in batch:
+    #         optimizer.zero_grad()
+    #         prediction = model(bg)
+    #         loss = torch.mean(F.cross_entropy(prediction, label, reduction='none')
+    #                          * (torch.ones_like(label, dtype=torch.float, device=device) +
+    #                             torch.tensor(label.clone().detach(), dtype=torch.float, device=device) * 10))
+    #
+    #         loss.backward()
+    #         optimizer.step()
+    #         epoch_loss += loss.detach().item()
+    #         batch.set_description(f'epoch {epoch} loss {epoch_loss}')
+    #     # if epoch > 10:
+    #     #     print(F.softmax(prediction.detach()).cpu())
+    #     #     print(label.cpu())
+    #
+    #     if epoch % 10 == 9:
+    #         model.eval()
+    #         epoch_loss = 0
+    #         dev_batch = tqdm(_dev_data_loader)
+    #         all_pred = []
+    #         all_label = []
+    #         with torch.no_grad():
+    #             for bg, label in dev_batch:
+    #                 prediction = model(bg)
+    #                 loss = torch.mean(F.cross_entropy(prediction, label, reduction='none')
+    #                                   * (torch.ones_like(label, dtype=torch.float, device=device) +
+    #                                      torch.tensor(label.clone().detach(), dtype=torch.float, device=device) * 10))
+    #
+    #                 all_pred.append(F.softmax(prediction.clone().detach()))
+    #                 all_label.append(label.clone().detach())
+    #
+    #                 dev_batch.set_description(f'-DEV- epoch {epoch} loss {epoch_loss}')
+    #         print(all_pred)
+    #         print(all_label)
     # train_data = get_data('gdb_9_clean.tsv', dataset='gdb-9', device='cuda:0')
     # data_loader = DataLoader(train_data, batch_size=32, shuffle=True, collate_fn=collate)
     # model = GCN(27, 16, 32, 11, 2, F.relu, 0.1)
@@ -156,3 +226,53 @@ if __name__ == '__main__':
     #     if epoch > 10:
     #         print(prediction.detach().cpu())
     #         print(label.cpu())
+    train_data = get_data_for_mlp('train_cv/fold_0/train.csv', device='cuda:0')
+    dev_data = get_data_for_mlp('train_cv/fold_0/dev.csv', device='cuda:0')
+    data_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+    _dev_data_loader = DataLoader(dev_data, batch_size=32, shuffle=False)
+
+    model = MLP(27 + 1, 16, 48, 2, 3, 0.1)
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    device = torch.device('cuda:0')
+    model.to(device)
+
+    for epoch in range(80):
+        model.train()
+        epoch_loss = 0
+        batch = tqdm(data_loader)
+        for elem, label, lengths in batch:
+            optimizer.zero_grad()
+            prediction = model(elem, lengths)
+            loss = torch.mean(F.cross_entropy(prediction, label, reduction='none')
+                              * (torch.ones_like(label, dtype=torch.float, device=device) +
+                                 torch.tensor(label.clone().detach(), dtype=torch.float, device=device) * 10))
+
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.detach().item()
+            batch.set_description(f'epoch {epoch} loss {epoch_loss}')
+        # if epoch > 10:
+        #     print(F.softmax(prediction.detach()).cpu())
+        #     print(label.cpu())
+
+        if epoch % 10 == 9:
+            model.eval()
+            epoch_loss = 0
+            dev_batch = tqdm(_dev_data_loader)
+            all_pred = []
+            all_label = []
+            with torch.no_grad():
+                for elem, label, lengths in dev_batch:
+                    prediction = model(elem, lengths)
+                    loss = torch.mean(F.cross_entropy(prediction, label, reduction='none')
+                                      * (torch.ones_like(label, dtype=torch.float, device=device) +
+                                         torch.tensor(label.clone().detach(), dtype=torch.float, device=device) * 10))
+                    epoch_loss = loss.item()
+                    all_pred.append(F.softmax(prediction.clone().detach()))
+                    all_label.append(label.clone().detach())
+
+                    dev_batch.set_description(f'-DEV- epoch {epoch} loss {epoch_loss}')
+            print(all_pred)
+            print(all_label)
--- utils.py
+++ utils.py
@@ -43,8 +43,8 @@ def extract_atom_feature(all_smiles, device):
 
 def read_raw(filename, dataset, device):
     if dataset == 'covid-19':
-        assertion_len = 3
-        smile_idx = 1
+        assertion_len = 2
+        smile_idx = 0
         separator = ','
     else:
         assertion_len = 13
@@ -55,7 +55,7 @@ def read_raw(filename, dataset, device):
     mols = []
     targets = []
     with open(filename) as f:
-        if smile_idx:
+        if assertion_len == 2:
             f.readline()
         f = tqdm(f)
         f.set_description('Reading raw data ... ')
@@ -70,7 +70,7 @@ def read_raw(filename, dataset, device):
                 smiles = Chem.MolToSmiles(m)
                 all_smiles.append(smiles)
 
-                targets.append(torch.tensor(int(l[2]), device=device) if smile_idx
+                targets.append(torch.tensor(int(l[1]), device=device) if assertion_len == 2
                                else torch.tensor([float(i) for i in l[2:]], device=device))
                 mol = read_smiles(smiles.replace('[H]', '[G]'), explicit_hydrogen=False, reinterpret_aromatic=True)
                 mols.append(mol)
@@ -135,17 +135,104 @@ def get_data(filename, dataset='covid-19', device='cpu'):
     return list(data)
 
 
+def get_data_for_mlp(filename, dataset='covid-19', device='cpu', max_len=100):
+    assert dataset.lower() in ['covid-19', 'gdb-9']
+    ele2idx = {'<PAD>': 0}
+
+    with open('ele2idx.txt', 'r') as f:
+        for line in f:
+            l = line.strip().split()
+            ele2idx[l[0]] = len(ele2idx)
+
+    # add unknown element into dictionary
+    ele2idx['<UNK>'] = len(ele2idx)
+
+    # all_molecules, targets, features = read_raw_for_mlp(filename, dataset, ele2idx, device, max_len)
+    all_molecules, targets, lengths = read_raw_for_mlp(filename, dataset, ele2idx, device, max_len)
+
+    #data = zip(all_molecules, features, targets)  # put all info related to each mol together
+    data = zip(all_molecules, targets, lengths)  # put all info related to each mol together
+    return list(data)
+
+
+def read_raw_for_mlp(filename, dataset, ele2idx, device, max_len):
+    if dataset == 'covid-19':
+        assertion_len = 2
+        smile_idx = 0
+        separator = ','
+    else:
+        assertion_len = 13
+        smile_idx = 0
+        separator = '\t'
+
+    all_smiles = []
+    mols = []
+    lengths = []
+    targets = []
+    with open(filename) as f:
+        if assertion_len == 2:
+            f.readline()
+        f = tqdm(f)
+        f.set_description('Reading raw data ... ')
+        for line in f:
+            if line != '':
+                l = line.strip().split(separator)
+
+                assert len(l) == assertion_len
+
+                m = Chem.MolFromSmiles(l[smile_idx])
+                m = Chem.AddHs(m)
+                smiles = Chem.MolToSmiles(m)
+                all_smiles.append(smiles)
+
+                targets.append(torch.tensor(int(l[1]), device=device) if assertion_len == 2
+                               else torch.tensor([float(i) for i in l[2:]], device=device))
+                mol = read_smiles(smiles.replace('[H]', '[G]'), explicit_hydrogen=False, reinterpret_aromatic=True)
+                mol = [ele2idx[prop['element']] for idx, prop in mol.nodes(data=True)]
+
+                if len(mol) <= max_len:
+                    lengths.append(torch.tensor(len(mol), dtype=torch.float, device=device))
+                    for i in range(max_len - len(mol)):
+                        mol.append(0)
+                else:
+                    mol = mol[:max_len]
+                    lengths.append(torch.tensor(max_len, dtype=torch.float, device=device))
+
+                mols.append(torch.tensor(mol, dtype=torch.long, device=device))
+
+    # features = extract_atom_feature(all_smiles, device)
+    return mols, targets, lengths #, features
+
+
 if __name__ == '__main__':
-    train_data = get_data('train.csv')
+    # train_data = get_data('train.csv')
     # print(list(train_data))
     # train_data = get_data('gdb_9_clean.tsv', dataset='gdb-9')
-    print(train_data)
+    # print(train_data)
     # print(extract_atom_feature_no_h('train.csv', 1, ','))
-    # m = Chem.MolFromSmiles('CON=C1CN(c2nc3c(cc2F)c(=O)c(C(=O)O)cn3C2CC2)CC1CN')
-    # m = Chem.AddHs(m)
-    # print(m.GetNumAtoms())
-    # print(Chem.MolToSmiles(m))
-    # print(list(read_smiles(Chem.MolToSmiles(m), explicit_hydrogen=True, reinterpret_aromatic=True).nodes(data=True)))
+    m = Chem.MolFromSmiles('CON=C1CN(c2nc3c(cc2F)c(=O)c(C(=O)O)cn3C2CC2)CC1CN')
+    m = Chem.AddHs(m)
+    print(m.GetNumAtoms())
+    print(Chem.MolToSmiles(m))
+    print(list(read_smiles(Chem.MolToSmiles(m), explicit_hydrogen=True, reinterpret_aromatic=True).nodes(data=True)))
     # print(list(read_smiles(Chem.MolToSmiles(m), explicit_hydrogen=True, reinterpret_aromatic=True).neighbors(48)))
     # print(list(read_smiles(Chem.MolToSmiles(m), explicit_hydrogen=True, reinterpret_aromatic=True).neighbors(7)))
     # print(list(read_smiles(Chem.MolToSmiles(m), explicit_hydrogen=True, reinterpret_aromatic=True).neighbors(17)))
+    # import os
+    # lines = []
+    # for folder in os.listdir('train_cv'):
+    #     with open('./train_cv/' + folder + '/dev.csv') as in_f:
+    #         if not len(lines):
+    #             lines.append(in_f.readline().strip())
+    #         for line in in_f:
+    #             lines.append(','.join(line.strip().split(',')))
+    #
+    # with open('dev.csv', 'w') as out_f:
+    #     out_f.write(lines[0])
+    #     out_f.write('\n')
+    #     lines = list(set(lines[1:]))
+    #     for idx, line in enumerate(lines):
+    #         out_f.write(str(idx))
+    #         out_f.write(',')
+    #         out_f.write(line)
+    #         out_f.write('\n')
